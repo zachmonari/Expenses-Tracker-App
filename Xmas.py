4fc5cdb9063a795b68c1393780126e03221eee70
zach <zachmonari002@gmail.com>
2025-12-03 18:45:26 +0300

    Page settings

--- Xmas.py
+++ Xmas.py
@@ -6,3 +6,19 @@ from PIL import Image
 
 logo=Image.open("ZachTechs.jpg")
 st.image(logo, width=300)
+
+# Page settings
+st.set_page_config(page_title="Merry Christmas 🎄", layout="centered")
+
+# Background Image CSS
+page_bg = """
+<style>
+[data-testid="stAppViewContainer"] {
+background-image: url("https://images.unsplash.com/photo-1606925797303-0c49b1e9c31d");
+background-size: cover;
+background-repeat: no-repeat;
+background-attachment: fixed;
+}
+</style>
+"""
+st.markdown(page_bg, unsafe_allow_html=True)
