5d40380e5aab9dc688baf7975d8321e4fa9d5a8c
zach <zachmonari002@gmail.com>
2025-12-03 18:44:26 +0300

    Added a logo

--- Xmas.py
+++ Xmas.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.let_it_rain import rain
-from streamlit_extras.emoji import emoji
+#from streamlit_extras.emoji import emoji
 from PIL import Image
+
+logo=Image.open("ZachTechs.jpg")
+st.image(logo, width=300)
